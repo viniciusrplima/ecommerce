51cba9a9c63425dd6b62f4bcab078bab708bdd1d
Vinicius Rodrigues <vinicius.rodrigues.lima@ccc.ufcg.edu.br>
2022-06-05 14:35:05 -0300

    refactor routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product/product-list/product-list.component';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductsComponent } from './views/products/products.component';
 
 const routes: Routes = [{
-  path: "products", 
-  component: ProductListComponent
-}, {
-  path: "product/:id", 
-  component: ProductReadComponent
+  path: "products",
+  component: ProductsComponent,
+  children: [{
+    path: "",
+    component: ProductListComponent
+  },
+  {
+    path: ":id",
+    component: ProductReadComponent
+  }]
 }];
 
 @NgModule({
